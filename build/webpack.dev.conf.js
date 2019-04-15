f1a231df2fe9cc9300005aa61cbcdaca3cfed381
dlgmlals3 <dlgmlals3@gmail.com>
2019-04-15 23:44:10 +0900

    set ip & port
    Change-Id: Ib249501c15773f9d523d8eec213e22eefd7fffff

--- build/webpack.dev.conf.js
+++ build/webpack.dev.conf.js
@@ -10,8 +10,10 @@ const HtmlWebpackPlugin = require('html-webpack-plugin')
 const FriendlyErrorsPlugin = require('friendly-errors-webpack-plugin')
 const portfinder = require('portfinder')
 
-const HOST = process.env.HOST
-const PORT = process.env.PORT && Number(process.env.PORT)
+//const HOST = process.env.HOST
+//const PORT = process.env.PORT && Number(process.env.PORT)
+const HOST = "218.38.52.30"
+const PORT = "8082"
 
 const devWebpackConfig = merge(baseWebpackConfig, {
   module: {
